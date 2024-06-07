2266f3bf4cb8163710397609ae04d958fea5bcbd
Fatih5252 <fatih.simsek5209@gmail.com>
2024-06-07 21:26:21 +0200

    bot

---
+++ README.md
@@ -0,0 +1,5 @@
+a discord.js giveaway bot! 
+just paste in src/functions/handelcommands line 5 your clientID
+fill all the required things in the .env file
+download all the packages in the requirements.txt file
+and you are done!
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require(`discord.js`);
+const fs = require('fs');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); 
+
+client.commands = new Collection();
+
+require('dotenv').config();
+
+const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./src/commands");
+
+(async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, "./src/events");
+    client.handleCommands(commandFolders, "./src/commands");
+    client.login(process.env.token)
+})();
+// Giveaway
+const GiveawaysManager = require('./giveaways');
+client.giveawayManager = new GiveawaysManager(client, {
+    default: {
+        botsCanWin: false,
+        embedColor: `0000ff`,
+        embedColorEnd: `0000ff`,
+        reaction: `🎉`,
+    },
+});
